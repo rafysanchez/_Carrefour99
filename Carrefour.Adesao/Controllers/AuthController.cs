8cb95e7c040b0386a6b07fe87e9a480aeaf88197
rafael sanchez port <rafysanchez@hotmail.com>
2020-02-24 18:59:17 -0300

    adjs

---
+++ Carrefour.Adesao/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carrefour.Adesao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        // GET: api/Adesao
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
